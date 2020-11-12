36f994c28ff13c385bd27e86b998c8450437678b
lagreenw <lagreenw@usc.edu>
2020-11-11 18:12:29 -0700

    Avatar Carousel - v1

---
+++ AvatarCarousel/AvatarCarousel.js
@@ -0,0 +1,10 @@
+//const avatar1 = document.getElementById('testAvatar');
+
+var avatar1 = document.createElement("IMG");
+
+avatar1.setAttribute("src", "testAvatar.png");
+avatar1.setAttribute("width", "100");
+avatar1.setAttribute("height", "100");
+avatar1.setAttribute("alt", "Test Avatar 1");
+avatar1.setAttribute("position", "absolute");
+document.body.appendChild(avatar1);
