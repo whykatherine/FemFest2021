f420ce1852d7103f1f01c614b096aeff72f63bde
lagreenw <lagreenw@usc.edu>
2020-11-12 19:16:51 -0700

    Avatar Carousel v4 (with 3 bounce types)

--- AvatarCarousel/AvatarCarousel.js
+++ AvatarCarousel/AvatarCarousel.js
@@ -24,7 +24,6 @@ for (var i = 0; i < 20; i++) {
 
   // randomize background
   var spin = getRandomInt(6);
-  console.log(spin);
   if (spin == 0) {
     avatars[i].style.background = "red";
   } else if (spin == 1) {
@@ -45,13 +44,13 @@ var timer = setInterval(releaseAvatar, 1500);
 var counter = 0;
 
 function releaseAvatar() {
-  console.log("ARRAY LENGTH: " + avatars.length);
-  console.log("COUNTER: " + counter);
+  // uncomment to add randomness to rate
+  clearInterval(timer);
+  timer = setInterval(releaseAvatar, ((getRandomInt(1000))+500));
+
   if (counter >= avatars.length) {
-    console.log("end of avatar array");
     counter = 0;
   } else {
-    console.log("AVATAR RELEASED");
     animContainer.appendChild(avatars[counter]);
     animate(counter);
     counter = counter + 1;
@@ -60,17 +59,66 @@ function releaseAvatar() {
 
 // Starts an avatar's animation when called with its index
 function animate(i) {
-  // Set equal to negative width of avatar
+  // Set xPos equal to negative width of avatar
   //var pos = -1*(avatars[i].getBoundingClientRect().width);
-  var pos = -100;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (pos >= 1920) {
-      clearInterval(id);
+  var xPos = -100;
+  var yPos = 0;
+  var yDir = "down";
+
+  // animate X position
+  var xTimer = setInterval(frameX, 10);
+  function frameX() {
+    if (xPos >= 1920) {
+      clearInterval(xTimer);
       animContainer.removeChild(avatars[i]);
     } else {
-      pos++;
-      avatars[i].style.left = pos + 'px';
+      xPos += 1;
+      avatars[i].style.left = xPos + 'px';
+    }
+  }
+
+  // low frame rate bounce
+  // var yTimer = setInterval(frameY, 1000);
+  // function frameY() {
+  //   if (yPos == 0) {
+  //     yPos = 50;
+  //     avatars[i].style.top = yPos + "px";
+  //     console.log("down");
+  //   } else {
+  //     console.log("up");
+  //     yPos = 0;
+  //     avatars[i].style.top = yPos + "px";
+  //   }
+  // }
+
+  // tweened bounce
+  // var yTimer = setInterval(frameY, 10);
+  // function frameY() {
+  //   if (yDir == "down") {
+  //     yPos++;
+  //     avatars[i].style.top = yPos + "px";
+  //     if (yPos >= 50) {
+  //       yDir = "up";
+  //     }
+  //   } else {
+  //     yPos--;
+  //     avatars[i].style.top = yPos + "px";
+  //     if (yPos <= 0) {
+  //       yDir = "down";
+  //     }
+  //   }
+  // }
+
+  // eased bounce
+  var yTimer = setInterval(frameY, 10);
+  var inc = 0;
+  var yPos = 50;
+  function frameY() {
+    yPos -= (1 + inc);
+    avatars[i].style.top = yPos + "px";
+    inc -= .02;
+    if (yPos > 50) {
+      inc = 0;
     }
   }
 }
