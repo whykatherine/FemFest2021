6befcc1edb9d0f76e2c9957133f485445bce8fd2
whykatherine <whykatherine@gmail.com>
2021-03-27 15:12:43 +0800

    feat: merch page

---
+++ src/pages/Merch.js
@@ -0,0 +1,59 @@
+// modules
+import React from 'react';
+
+function Merch(props) {
+	let merch = {
+		"headline": {
+			"Caroline Polachek": {},
+			"Dorian Electra": {},
+			"Raveena": {}
+		},
+		"supporting": {
+			"Yves Tumor": {},
+			"Eartheater": {},
+			"Fraxiom": {},
+			"Caro♥": {}
+		},
+		"speakers": {
+			"Mykki Blanco": {},
+			"Funmilola Fagbamila": {}
+		},
+		"performers": {
+			"Vander Von Odd": {},
+			"Abhora": {},
+			"Freckle": {}
+		},
+		"designers": {
+			"Nina Doll": {},
+			"Marco Alcala": {},
+			"Hana Pua": {},
+			"Kris Yuan": {},
+			"Shroonchu": {},
+			"Aylish Turner": {}
+		}
+	};
+
+	return (
+		<div className="marketplace">
+      
+      <div className="text">
+        <h1 data-content="Merch">Merch</h1>
+      </div>
+
+      <div className="gallery gallery--merch">
+        {Object.keys(merch).map(cat => (
+        	<div className="gallery_section" key={cat}>
+        		{Object.keys(merch[cat]).map(name => (
+        			<div className="gallery__item" key={name}>
+		            <p>{name}</p>
+		          </div>
+      			))}
+        	</div>
+        ))}
+      </div>
+
+    </div>
+	);
+}
+
+export default Merch;
