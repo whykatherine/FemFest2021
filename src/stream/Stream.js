082b987fcdfd91adc1b5cf7f3eeba7f855bc7796
whykatherine <whykatherine@gmail.com>
2021-03-27 09:00:07 +0800

    fix: update twitch channel

--- src/stream/Stream.js
+++ src/stream/Stream.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Stream(props) {
   return (
-    <iframe src="https://player.twitch.tv/?channel=hasanabi&parent=femfest.online"
+    <iframe src="https://player.twitch.tv/?channel=femfest&parent=femfest.online"
       frameBorder="0" allowFullScreen={true} scrolling="no" height="378" width="620"
       id="stream_embed"
       title="Twitch stream"></iframe>
