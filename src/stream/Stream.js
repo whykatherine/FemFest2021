70b7bebbceb5675a1c28291ee2f9c3440cf9691d
whykatherine <whykatherine@gmail.com>
2021-03-27 16:27:19 +0800

    fix: put back stream

--- src/stream/Stream.js
+++ src/stream/Stream.js
@@ -2,13 +2,11 @@ import React from 'react';
 
 function Stream(props) {
   return (
-  	<div></div>
-  );
-
-  /*<iframe src="https://player.twitch.tv/?channel=femfest&parent=femfest.online"
+  	<iframe src="https://player.twitch.tv/?channel=femfest&parent=femfest.online"
       frameBorder="0" allowFullScreen={true} scrolling="no" height="378" width="620"
       id="stream_embed"
-      title="Twitch stream"></iframe>*/
+      title="Twitch stream"></iframe>
+  );
 }
 
 export default Stream;
--- src/stream/StreamChat.js
+++ src/stream/StreamChat.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
 function StreamChat(props) {
-  // <div id="chat_embed__container">
   return (
-  	<div></div>
+  	<iframe
+      id="chat_embed"
+      src="https://www.twitch.tv/embed/femfest/chat?parent=femfest.online"
+      height="500"
+      width="350"
+      title="Twitch stream chat"></iframe>
   );
-  /*<iframe
-        id="chat_embed"
-        src="https://www.twitch.tv/embed/femfest/chat?parent=femfest.online"
-        height="500"
-        width="350"
-        title="Twitch stream chat"></iframe>*/
 }
 
 export default StreamChat;
