f3eafe3b35dad896fb573b4bec592eeb1c27c2a2
whykatherine <whykatherine@gmail.com>
2021-04-01 13:27:40 +0800

    fix: stream embeds

--- src/windows/Stream.js
+++ src/windows/Stream.js
@@ -1,12 +1,11 @@
 import React from 'react';
 
 function Stream(props) {
-	{/*<iframe src="https://player.twitch.tv/?channel=femfest&parent=femfest.online"
+  return (
+  	<iframe src="https://player.twitch.tv/?channel=femfest&parent=femfest.online"
       frameBorder="0" allowFullScreen={true} scrolling="no" height="378" width="620"
       id="stream_embed"
-      title="Twitch stream"></iframe>*/}
-  return (
-  	<div></div>
+      title="Twitch stream"></iframe>
   );
 }
 
--- src/windows/StreamChat.js
+++ src/windows/StreamChat.js
@@ -1,14 +1,13 @@
 import React from 'react';
 
 function StreamChat(props) {
-	{/*<iframe
+  return (
+  	<iframe
       id="chat_embed"
       src="https://www.twitch.tv/embed/femfest/chat?parent=femfest.online"
       height="500"
       width="350"
-      title="Twitch stream chat"></iframe>*/}
-  return (
-  	<div></div>
+      title="Twitch stream chat"></iframe>
   );
 }
 
